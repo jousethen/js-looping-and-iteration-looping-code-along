a2fd27852eb0aa27438155849646450490cab024
jousethen <jthen93@gmail.com>
2021-08-04 13:27:50 -0400

    commit tree e7366c4fe9778dc55ccce52747b1ee2a5333bb25

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solutions in this file
+function writeCards(arr, eventName) {
+
+  let eventArr = [];
+  for (let i = 0; i < arr.length; i++) {
+    eventArr.push(`Thank you, ${arr[i]}, for the wonderful ${eventName} gift!`);
+  }
+  return eventArr;
+}
+
+function countDown(number) {
+  let i = number;
+
+  while (i >= 0) {
+    console.log(i);
+    i--;
+  }
+}
